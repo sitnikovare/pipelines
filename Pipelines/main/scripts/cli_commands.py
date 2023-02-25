5adb0eec83c4186818ab6a57019b75d0d4c7eb69
sitnikovare <sitnikovare@gmail.com>
2023-02-25 06:48:41 +0200

    CLI description

--- Pipelines/main/scripts/cli_commands.py
+++ Pipelines/main/scripts/cli_commands.py
@@ -4,17 +4,20 @@ from main.pipelines_scripts import main_commands
 
 @click.group()
 def pipeline():
+    """ Working with pipeline. """
     pass
 
 
 @pipeline.command()
 def run():
     # click.echo("CLICK: command run started")
+    """ - Run pipeline in current directory. """
     main_commands.pipelines_run()
 
 
 @pipeline.command()
 def list():
     # click.echo("CLICK: command list started")
+    """ - Get list of pipelines. """
     main_commands.pipelines_list()
 
